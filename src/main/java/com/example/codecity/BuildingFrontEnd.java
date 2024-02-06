acf8b100cb0232c84dbc1244b5c6b296583d925d
Miro Haapalainen <32234658+Merplop@users.noreply.github.com>
2024-02-05 21:09:41 -0800

    Update BuildingFrontEnd.java

--- src/main/java/com/example/codecity/BuildingFrontEnd.java
+++ src/main/java/com/example/codecity/BuildingFrontEnd.java
@@ -1,23 +1,52 @@
-package com.example.codecityjavafx;
+    package com.example.codecityjavafx;
 
-import javafx.scene.paint.Color;
+    import javafx.scene.paint.Color;
 
-public class BuildingFrontEnd  {
-    private final int x;
-    private final int y;
-    private final Color color;
-    private final int height;
+    /**
+     * Class handling the attributes of the front-end graphical representations of the classes
+     */
 
-    public BuildingFrontEnd(int x, int y, Color c, int height) {
-        this.x = x;
-        this.y = y;
-        this.color = c;
-        this.height = height;
+    public class BuildingFrontEnd  {
+        private final int x;
+        private final int y;
+        private final Color color;
+        private final int height;
+
+        /**
+         *  Class constructor
+         * @param x X-position of building on canvas
+         * @param y Y-position of building on canvas
+         * @param c Color to draw building
+         * @param height Height of building
+         */
+
+        public BuildingFrontEnd(int x, int y, Color c, int height) {
+            this.x = x;
+            this.y = y;
+            this.color = c;
+            this.height = height;
+        }
+
+        /**
+         *
+         * @return X position of building on screen
+         */
+
+        public int getX() {return x;}
+        /**
+         *
+         * @return Y position of building on screen
+         */
+        public int getY() {return y;}
+        /**
+         *
+         * @return Color of building on screen
+         */
+        public Color getColor() {return color;}
+        /**
+         *
+         * @return Height of building on screen
+         */
+
+        public int getHeight() {return height;}
     }
-
-    public int getX() {return x;}
-    public int getY() {return y;}
-    public Color getColor() {return color;}
-
-    public int getHeight() {return height;}
-}
